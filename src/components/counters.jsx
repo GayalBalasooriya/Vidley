a14097dad80ec3363739154b56d0a388b11e3842
Gayal Balasooriya <baigayal01@gmail.com>
2021-12-28 19:50:43 +0530

    Local state removed

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,18 +3,59 @@ import Counter from "./counter";
 
 class Counters extends React.Component {
   state = {
-    key: 0,
-    count: 4,
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    //console.log("Event handler called", counterId);
+    const counters = this.state.counters.filter((i) => i.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((i) => {
+      i.value = 0;
+      return i;
+    });
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
-        {this.props.children}
-        <Counter key={this.state.key} value={this.state.count}>
-          <h4>Hi</h4>
-          <h1>Name</h1>
-        </Counter>
+        <button
+          className="btn-btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            //value={counter.value}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            //id={counter.id}
+            //selected={true}
+            counter={counter}
+          >
+            <h4>Counter #{counter.id}</h4>
+            <h1>Name</h1>
+          </Counter>
+        ))}
       </div>
     );
   }
