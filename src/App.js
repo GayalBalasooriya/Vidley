c0546fc72fcb57f6acfa454679e6817e02cf85b2
Gayal Balasooriya <baigayal01@gmail.com>
2021-12-29 12:54:22 +0530

    Lifting state up done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import './App.css';
-import Movies from './components/movies';
+//import Movies from './components/movies';
+import Counters from './components/counters';
+import NavBar from './components/navbar';
+import React from 'react';
 
-function App() {
-  return (
-    <main className="container">
-      <Movies />
-    </main>
-  );
+class App extends React.Component {
+
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    //console.log("Event handler called", counterId);
+    const counters = this.state.counters.filter((i) => i.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((i) => {
+      i.value = 0;
+      return i;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c=>c.value>0).length}/>
+        <main className="container">
+          <Counters counters={this.state.counters} onReset={this.handleReset} onDelete={this.handleDelete} onIncrement={this.handleIncrement}/>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,52 +2,52 @@ import React from "react";
 import Counter from "./counter";
 
 class Counters extends React.Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
+  // state = {
+  //   counters: [
+  //     { id: 1, value: 4 },
+  //     { id: 2, value: 0 },
+  //     { id: 3, value: 0 },
+  //     { id: 4, value: 0 },
+  //   ],
+  // };
 
-  handleDelete = (counterId) => {
-    //console.log("Event handler called", counterId);
-    const counters = this.state.counters.filter((i) => i.id !== counterId);
-    this.setState({ counters });
-  };
+  // handleDelete = (counterId) => {
+  //   //console.log("Event handler called", counterId);
+  //   const counters = this.state.counters.filter((i) => i.id !== counterId);
+  //   this.setState({ counters });
+  // };
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
+  // handleIncrement = (counter) => {
+  //   const counters = [...this.state.counters];
+  //   const index = counters.indexOf(counter);
+  //   counters[index] = { ...counter };
+  //   counters[index].value++;
+  //   this.setState({ counters });
+  // };
 
-  handleReset = () => {
-    const counters = this.state.counters.map((i) => {
-      i.value = 0;
-      return i;
-    });
-    this.setState({ counters });
-  };
+  // handleReset = () => {
+  //   const counters = this.state.counters.map((i) => {
+  //     i.value = 0;
+  //     return i;
+  //   });
+  //   this.setState({ counters });
+  // };
 
   render() {
     return (
       <div>
         <button
           className="btn-btn-primary btn-sm m-2"
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
             //value={counter.value}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             //id={counter.id}
             //selected={true}
             counter={counter}
